864d3ef148bed89b47eccda976f94fd66315613b
alexspolet <lelikpolet@gmail.com>
2018-11-26 10:37:30 +0300

    lesson5

--- index.php
+++ index.php
@@ -7,13 +7,18 @@
  */
 session_start();
 
+function __autoload($name){
+  require_once str_replace('\\' , DIRECTORY_SEPARATOR , $name). '.php';
+}
+
 require_once 'Models/system_m.php';
-require_once 'Models/ArticleModel.php';
+//require_once 'Models/ArticleModel.php';
 require_once 'Models/global_vars.php';
 
 $auth = isAuth();
-$db = connectDb();
-$articles = getAllArticles($db);
+
+$mArticle = \Models\ArticleModel::getInstance();
+$articles = $mArticle->getAll();
 
 $path = getPath();
 $content = renderHtml($path, [
--- view/delete_v.php
+++
@@ -1,8 +0,0 @@
-<?if (isset($error)):?>
-<p><?=$error;?></p>
-<p><a href="<?=$mainfile?>">To the main file</a></p>
-<?else:?>
-  <p>File was deleted successfully</p>
-  <p><a href="<?=$mainfile?>">To the main file</a></p>
-<?endif;?>
-
