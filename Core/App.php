b900e65e2558bfa36cdb9ec640821f54a6481414
alexspolet <lelikpolet@gmail.com>
2018-11-28 00:50:03 +0300

    class App almost done

--- Core/App.php
+++ Core/App.php
@@ -9,7 +9,60 @@
 namespace Core;
 
 
+use Controllers\ArticleController;
+
 class App
 {
 
+  private $request;
+
+  /**
+   * App constructor.
+   */
+  public function __construct(Request $request)
+  {
+    $this->request = $request;
+  }
+
+  public function goApp(){
+    if (!$this->getRouteByRequest()){
+      $ctrl = new ''
+    }
+  }
+
+  private function getRouteByRequest(){
+    return isset($this->getRoutes()[$this->request->getRoute()]) ? $this->getRoutes()[$this->request->getRoute()] : false;
+  }
+
+  private function getRoutes()
+  {
+    return
+        ['/' =>
+            [
+                'controller' => 'Controllers\ArticleController',
+                'action' => 'indexAction'
+            ],
+            '/article' =>
+                [
+                    'controller' => 'Controllers\ArticleController',
+                    'action' => 'indexAction'
+                ],
+            '/add' =>
+                [
+                    'controller' => 'Controllers\ArticleController',
+                    'action' => 'addAction'
+                ],
+            '/edit' =>
+                [
+                    'controller' => 'Controllers\ArticleController',
+                    'action' => 'editAction'
+                ],
+            '/delete' =>
+                [
+                    'controller' => 'Controllers\ArticleController',
+                    'action' => 'deleteAction'
+                ],
+
+        ];
+  }
 }
